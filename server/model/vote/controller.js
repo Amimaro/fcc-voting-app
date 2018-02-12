82abf2fc1e50df26eacb92dfd7a42b2e6973cb6d
Amimaro <amir.zahlan@gmail.com>
2018-02-12 12:16:54 -0300

    Remove vote model

--- server/model/vote/controller.js
+++
@@ -1,6 +0,0 @@
-const Controller = require('../../lib/controller');
-const voteFacade = require('./facade');
-
-class VoteController extends Controller {}
-
-module.exports = new VoteController(voteFacade);
--- server/model/vote/facade.js
+++
@@ -1,6 +0,0 @@
-const Facade = require('../../lib/facade');
-const voteSchema = require('./schema');
-
-class VoteFacade extends Facade {}
-
-module.exports = new VoteFacade(voteSchema);
